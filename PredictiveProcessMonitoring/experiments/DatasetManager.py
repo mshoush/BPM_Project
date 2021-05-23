383559ad4297194eb7b53c8a37e72c6c28e9e210
Gamal Elkoumy <gamal.elkomy@gmail.com>
2021-05-23 13:59:31 +0300

    predictive process monitoring

---
+++ PredictiveProcessMonitoring/experiments/DatasetManager.py
@@ -0,0 +1,209 @@
+import sys
+
+import dataset_confs
+
+import pandas as pd
+import numpy as np
+
+from sklearn.model_selection import StratifiedKFold
+
+def create_ngrams(data, ngram_size):
+    result=pd.DataFrame()
+
+
+    for idx in range(0,data.shape[0]- ngram_size +1):
+
+        prefix=data.iloc[idx:idx+ngram_size].copy()
+        prefix=prefix.reset_index()
+
+        prefix['Case ID']=prefix['Case ID']+'_'+str(idx)
+        prefix['prefix_nr']=idx+1
+        result=pd.concat([result,prefix])
+
+    return result
+
+
+class DatasetManager:
+    
+    def __init__(self, dataset_name):
+        self.dataset_name = dataset_name
+        
+        self.case_id_col = dataset_confs.case_id_col[self.dataset_name]
+        self.activity_col = dataset_confs.activity_col[self.dataset_name]
+        self.timestamp_col = dataset_confs.timestamp_col[self.dataset_name]
+        self.label_col = dataset_confs.label_col[self.dataset_name]
+        self.pos_label = dataset_confs.pos_label[self.dataset_name]
+
+        self.dynamic_cat_cols = dataset_confs.dynamic_cat_cols[self.dataset_name]
+        self.static_cat_cols = dataset_confs.static_cat_cols[self.dataset_name]
+        self.dynamic_num_cols = dataset_confs.dynamic_num_cols[self.dataset_name]
+        self.static_num_cols = dataset_confs.static_num_cols[self.dataset_name]
+        
+        self.sorting_cols = [self.timestamp_col, self.activity_col]
+        
+    
+    def read_dataset(self):
+        # read dataset
+        dtypes = {col:"object" for col in self.dynamic_cat_cols+self.static_cat_cols+[self.case_id_col, self.label_col, self.timestamp_col]}
+        for col in self.dynamic_num_cols + self.static_num_cols:
+            dtypes[col] = "float"
+
+        data = pd.read_csv(dataset_confs.filename[self.dataset_name], sep=";", dtype=dtypes)
+        print(data.columns)
+        data[self.timestamp_col] = pd.to_datetime(data[self.timestamp_col])
+
+        return data
+
+
+    def split_data(self, data, train_ratio, split="temporal", seed=22):  
+        # split into train and test using temporal split
+
+        grouped = data.groupby(self.case_id_col)
+        start_timestamps = grouped[self.timestamp_col].min().reset_index()
+        if split == "temporal":
+            start_timestamps = start_timestamps.sort_values(self.timestamp_col, ascending=True, kind="mergesort")
+        elif split == "random":
+            np.random.seed(seed)
+            start_timestamps = start_timestamps.reindex(np.random.permutation(start_timestamps.index))
+        train_ids = list(start_timestamps[self.case_id_col])[:int(train_ratio*len(start_timestamps))]
+        train = data[data[self.case_id_col].isin(train_ids)].sort_values(self.timestamp_col, ascending=True, kind='mergesort')
+        test = data[~data[self.case_id_col].isin(train_ids)].sort_values(self.timestamp_col, ascending=True, kind='mergesort')
+
+        return (train, test)
+    
+    def split_data_strict(self, data, train_ratio, split="temporal"):  
+        # split into train and test using temporal split and discard events that overlap the periods
+        data = data.sort_values(self.sorting_cols, ascending=True, kind='mergesort')
+        grouped = data.groupby(self.case_id_col)
+        start_timestamps = grouped[self.timestamp_col].min().reset_index()
+        start_timestamps = start_timestamps.sort_values(self.timestamp_col, ascending=True, kind='mergesort')
+        train_ids = list(start_timestamps[self.case_id_col])[:int(train_ratio*len(start_timestamps))]
+        train = data[data[self.case_id_col].isin(train_ids)].sort_values(self.sorting_cols, ascending=True, kind='mergesort')
+        test = data[~data[self.case_id_col].isin(train_ids)].sort_values(self.sorting_cols, ascending=True, kind='mergesort')
+        split_ts = test[self.timestamp_col].min()
+        train = train[train[self.timestamp_col] < split_ts]
+        return (train, test)
+    
+    def split_data_discard(self, data, train_ratio, split="temporal"):  
+        # split into train and test using temporal split and discard events that overlap the periods
+        data = data.sort_values(self.sorting_cols, ascending=True, kind='mergesort')
+        grouped = data.groupby(self.case_id_col)
+        start_timestamps = grouped[self.timestamp_col].min().reset_index()
+        start_timestamps = start_timestamps.sort_values(self.timestamp_col, ascending=True, kind='mergesort')
+        train_ids = list(start_timestamps[self.case_id_col])[:int(train_ratio*len(start_timestamps))]
+        train = data[data[self.case_id_col].isin(train_ids)].sort_values(self.sorting_cols, ascending=True, kind='mergesort')
+        test = data[~data[self.case_id_col].isin(train_ids)].sort_values(self.sorting_cols, ascending=True, kind='mergesort')
+        split_ts = test[self.timestamp_col].min()
+        overlapping_cases = train[train[self.timestamp_col] >= split_ts][self.case_id_col].unique()
+        train = train[~train[self.case_id_col].isin(overlapping_cases)]
+        return (train, test)
+    
+    
+    def split_val(self, data, val_ratio, split="random", seed=22):  
+        # split into train and test using temporal split
+        grouped = data.groupby(self.case_id_col)
+        start_timestamps = grouped[self.timestamp_col].min().reset_index()
+        if split == "temporal":
+            start_timestamps = start_timestamps.sort_values(self.timestamp_col, ascending=True, kind="mergesort")
+        elif split == "random":
+            np.random.seed(seed)
+            start_timestamps = start_timestamps.reindex(np.random.permutation(start_timestamps.index))
+        val_ids = list(start_timestamps[self.case_id_col])[-int(val_ratio*len(start_timestamps)):]
+        val = data[data[self.case_id_col].isin(val_ids)].sort_values(self.sorting_cols, ascending=True, kind="mergesort")
+        train = data[~data[self.case_id_col].isin(val_ids)].sort_values(self.sorting_cols, ascending=True, kind="mergesort")
+        return (train, val)
+
+
+    def generate_prefix_data_old(self, data, min_length, max_length, gap=1):
+        # generate prefix data (each possible prefix becomes a trace)
+        data['case_length'] = data.groupby(self.case_id_col)[self.activity_col].transform(len)
+
+        dt_prefixes = data[data['case_length'] >= min_length].groupby(self.case_id_col).head(min_length)
+        dt_prefixes["prefix_nr"] = 1
+        dt_prefixes["orig_case_id"] = dt_prefixes[self.case_id_col]
+        for nr_events in range(min_length+gap, max_length+1, gap):
+            tmp = data[data['case_length'] >= nr_events].groupby(self.case_id_col).head(nr_events)
+            tmp["orig_case_id"] = tmp[self.case_id_col]
+            tmp[self.case_id_col] = tmp[self.case_id_col].apply(lambda x: "%s_%s"%(x, nr_events))
+            tmp["prefix_nr"] = nr_events
+            dt_prefixes = pd.concat([dt_prefixes, tmp], axis=0)
+        
+        dt_prefixes['case_length'] = dt_prefixes['case_length'].apply(lambda x: min(max_length, x))
+        
+        return dt_prefixes
+
+    def generate_prefix_data(self,data, ngram_size):
+        # generate prefix data (each possible prefix becomes a trace)
+
+        # ngram_size=3
+        dt_prefixes=data.groupby(['Case ID']).apply(create_ngrams, ngram_size)
+
+        dt_prefixes=dt_prefixes.rename(columns={'Case ID': 'newcaseid'})
+        dt_prefixes=dt_prefixes.reset_index().rename(columns={'Case ID': 'original_caseid'})
+        dt_prefixes=dt_prefixes.drop('level_1',axis=1)
+        dt_prefixes=dt_prefixes.rename(columns={'newcaseid': 'Case ID'})
+
+        return dt_prefixes
+
+    def generate_prefix_data_ngram(self,data, ngram_size):
+        # generate prefix data (each possible prefix becomes a trace)
+
+        # ngram_size=3
+        dt_prefixes=data.groupby(['Case ID']).apply(create_ngrams, ngram_size)
+
+        dt_prefixes=dt_prefixes.rename(columns={'Case ID': 'newcaseid'})
+        dt_prefixes=dt_prefixes.reset_index().rename(columns={'Case ID': 'original_caseid'})
+        dt_prefixes=dt_prefixes.drop('level_1',axis=1)
+        dt_prefixes=dt_prefixes.rename(columns={'newcaseid': 'Case ID'})
+
+        return dt_prefixes
+    def get_pos_case_length_quantile(self, data, quantile=0.90):
+        print("result is")
+        print(self.label_col)
+        print(self.pos_label)
+        return int(np.ceil(data[data[self.label_col]==self.pos_label].groupby(self.case_id_col).size().quantile(quantile)))
+
+    def get_indexes(self, data):
+        return data.groupby(self.case_id_col).first().index
+
+    def get_relevant_data_by_indexes(self, data, indexes):
+        return data[data[self.case_id_col].isin(indexes)]
+
+    def get_label(self, data):
+        return data.groupby(self.case_id_col).first()[self.label_col]
+    
+    def get_prefix_lengths(self, data):
+        return data.groupby(self.case_id_col).last()["prefix_nr"]
+    
+    def get_case_ids(self, data, nr_events=1):
+        case_ids = pd.Series(data.groupby(self.case_id_col).first().index)
+        if nr_events > 1:
+            case_ids = case_ids.apply(lambda x: "_".join(x.split("_")[:-1]))
+        return case_ids
+    
+    def get_label_numeric(self, data):
+        y = self.get_label(data) # one row per case
+        return [1 if label == self.pos_label else 0 for label in y]
+    
+    def get_class_ratio(self, data):
+        class_freqs = data[self.label_col].value_counts()
+        return class_freqs[self.pos_label] / class_freqs.sum()
+    
+    def get_stratified_split_generator(self, data, n_splits=5, shuffle=True, random_state=22):
+        grouped_firsts = data.groupby(self.case_id_col, as_index=False).first()
+        skf = StratifiedKFold(n_splits=n_splits, shuffle=shuffle, random_state=random_state)
+        
+        for train_index, test_index in skf.split(grouped_firsts, grouped_firsts[self.label_col]):
+            current_train_names = grouped_firsts[self.case_id_col][train_index]
+            train_chunk = data[data[self.case_id_col].isin(current_train_names)].sort_values(self.timestamp_col, ascending=True, kind='mergesort')
+            test_chunk = data[~data[self.case_id_col].isin(current_train_names)].sort_values(self.timestamp_col, ascending=True, kind='mergesort')
+            yield (train_chunk, test_chunk)
+            
+    def get_idx_split_generator(self, dt_for_splitting, n_splits=5, shuffle=True, random_state=22):
+        skf = StratifiedKFold(n_splits=n_splits, shuffle=shuffle, random_state=random_state)
+        
+        for train_index, test_index in skf.split(dt_for_splitting, dt_for_splitting[self.label_col]):
+            current_train_names = dt_for_splitting[self.case_id_col][train_index]
+            current_test_names = dt_for_splitting[self.case_id_col][test_index]
+            yield (current_train_names, current_test_names)
+            
---
+++ PredictiveProcessMonitoring/experiments/project_prediction.py
@@ -0,0 +1,168 @@
+import EncoderFactory
+from DatasetManager import DatasetManager
+import BucketFactory
+
+import pandas as pd
+import numpy as np
+
+from sklearn.metrics import roc_auc_score
+from sklearn.pipeline import FeatureUnion, Pipeline
+
+import os
+
+import pickle
+
+import xgboost as xgb
+
+
+dataset_ref = "DahnaLoanApplication_prepared"
+params_dir = "optimizer_log"
+results_dir = "experiment_log"
+bucket_method = "cluster"
+cls_encoding = "index"
+cls_method = "xgboost"
+ngram_size = 4
+
+bucket_encoding = "agg"
+
+method_name = "%s_%s" % (bucket_method, cls_encoding)
+
+
+
+encoding_dict = {
+    "laststate": ["static", "last"],
+    "agg": ["static", "agg"],
+    "index": ["static", "index"],
+    "combined": ["static", "last", "agg"]
+}
+
+
+methods = encoding_dict[cls_encoding]
+
+train_ratio = 0.8
+random_state = 22
+
+# create results directory
+if not os.path.exists(os.path.join(params_dir)):
+    os.makedirs(os.path.join(params_dir))
+
+dataset_name=dataset_ref
+
+
+# read the data
+dataset_manager = DatasetManager(dataset_name)
+data = dataset_manager.read_dataset()
+cls_encoder_args = {'case_id_col': dataset_manager.case_id_col,
+                    'static_cat_cols': dataset_manager.static_cat_cols,
+                    'static_num_cols': dataset_manager.static_num_cols,
+                    'dynamic_cat_cols': dataset_manager.dynamic_cat_cols,
+                    'dynamic_num_cols': dataset_manager.dynamic_num_cols,
+                    'fillna': True}
+
+
+# split into training and test
+train, test = dataset_manager.split_data_strict(data, train_ratio, split="temporal")
+
+dt_test_prefixes = dataset_manager.generate_prefix_data(test, ngram_size)
+
+
+# create prefix logs
+
+dt_train_prefixes = dataset_manager.generate_prefix_data(train, ngram_size)
+
+
+# Bucketing prefixes based on control flow
+bucketer_args = {'encoding_method': bucket_encoding,
+                 'case_id_col': dataset_manager.case_id_col,
+                 'cat_cols': [dataset_manager.activity_col],
+                 'num_cols': [],
+                 'random_state': random_state}
+
+
+# load optimal params
+optimal_params_filename = os.path.join(params_dir,
+                                       "optimal_params_%s_%s_%s.pickle" % (cls_method, dataset_name, method_name))
+
+with open(optimal_params_filename, "rb") as fin:
+    args = pickle.load(fin)
+
+if bucket_method == "cluster":
+    bucketer_args["n_clusters"] = int(args["n_clusters"])
+bucketer = BucketFactory.get_bucketer(bucket_method, **bucketer_args)
+
+
+bucket_assignments_train = bucketer.fit_predict(dt_train_prefixes)
+
+bucket_assignments_test = bucketer.predict(dt_test_prefixes)
+
+preds_all = []
+test_y_all = []
+nr_events_all = []
+
+for bucket in set(bucket_assignments_test):
+    if bucket_method == "prefix":
+        current_args = args[bucket]
+    else:
+        current_args = args
+    relevant_train_cases_bucket = dataset_manager.get_indexes(dt_train_prefixes)[
+        bucket_assignments_train == bucket]
+    relevant_test_cases_bucket = dataset_manager.get_indexes(dt_test_prefixes)[
+        bucket_assignments_test == bucket]
+    dt_test_bucket = dataset_manager.get_relevant_data_by_indexes(dt_test_prefixes, relevant_test_cases_bucket)
+
+    nr_events_all.extend(list(dataset_manager.get_prefix_lengths(dt_test_bucket)))
+    if len(relevant_train_cases_bucket) == 0:
+        preds = [dataset_manager.get_class_ratio(train)] * len(relevant_test_cases_bucket)
+
+    else:
+        dt_train_bucket = dataset_manager.get_relevant_data_by_indexes(dt_train_prefixes,
+                                                                       relevant_train_cases_bucket)  # one row per event
+        train_y = dataset_manager.get_label_numeric(dt_train_bucket)
+
+        if len(set(train_y)) < 2:
+            preds = [train_y[0]] * len(relevant_test_cases_bucket)
+
+            test_y_all.extend(dataset_manager.get_label_numeric(dt_test_bucket))
+        else:
+
+            feature_combiner = FeatureUnion(
+                [(method, EncoderFactory.get_encoder(method, **cls_encoder_args)) for method in methods])
+
+
+            cls = xgb.XGBClassifier(objective='binary:logistic',
+                                    n_estimators=500,
+                                    learning_rate=current_args['learning_rate'],
+                                    subsample=current_args['subsample'],
+                                    max_depth=int(current_args['max_depth']),
+                                    colsample_bytree=current_args['colsample_bytree'],
+                                    min_child_weight=int(current_args['min_child_weight']),
+                                    seed=random_state)
+
+            pipeline = Pipeline([('encoder', feature_combiner), ('cls', cls)])
+
+            pipeline.fit(dt_train_bucket, train_y)
+
+
+            # predict separately for each prefix case
+            preds = []
+            test_all_grouped = dt_test_bucket.groupby(dataset_manager.case_id_col)
+            for _, group in test_all_grouped:
+
+                test_y_all.extend(dataset_manager.get_label_numeric(group))
+
+
+                _ = bucketer.predict(group)
+
+                preds_pos_label_idx = np.where(cls.classes_ == 1)[0][0]
+                pred = pipeline.predict_proba(group)[:, preds_pos_label_idx]
+
+
+                preds.extend(pred)
+
+    preds_all.extend(preds)
+
+
+dt_results = pd.DataFrame({"actual": test_y_all, "predicted": preds_all, "nr_events": nr_events_all})
+
+print("The AUC is: %s\n" % (roc_auc_score(dt_results.actual, dt_results.predicted)))
+
